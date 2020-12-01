106d6b9341e8fe42be97a40ec26b9d84bef71063
Aditya Chakraborty <adityarc19@gmail.com>
2020-12-01 20:26:12 +0530

    Add files via upload

---
+++ analysis.R
@@ -0,0 +1,26 @@
+# library
+library(covid19.analytics)    #gets data from the John Hopkins github repo for covid19 data
+
+# data
+ag <- covid19.data(case = 'aggregated')
+View(ag)
+tsc <- covid19.data(case = 'ts-confirmed')   #Confirmed cases time series data
+                                             #Range of dates on data: 2020-01-22 to 2020-11-30 
+                                             #Nbr of records: 271
+View(tsc)
+
+# Summary
+report.summary(Nentries = 10,
+               graphical.output = T)
+
+
+# Growth rate
+growth.rate(tsc, geo.loc = 'India')
+
+# Totals plot
+totals.plt(tsc, c('India'))
+
+# SIR Model
+generate.SIR.model(tsc, 'India', tot.population = 1380004385)
+
+
